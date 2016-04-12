c1beff39748d06478a5b7e2fe5fe3b4cee73d5fc
aruethe2 <aruethe2@swarthmore.edu>
2016-04-12 09:52:53 -0400

    Added circuit diagrams

--- config_dist.h
+++ config_dist.h
@@ -1,4 +1,4 @@
-// EDIT THE LINES BELOW WITH YOUR INFORMATION
+// EDIT THE LINES BELOW WITH YOUR INFORMATION AMD SAVE AS config.h
 
 // Ethernet board/ahield MAC address (see the sticker on the board)
 #define ARDUINO_ETHERNET_MAC { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
